a1167ef476741ef59a4fc503e0cfa6ab7cde49e1
Benjamin Lannon <benjamin@lannonbr.com>
2018-08-20 20:41:28 -0400

    0.2.1 - Small fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.1 - Aug 20, 2018
+- [Hotfix] Added keybinding for toggling annotations ('ctrl/cmd+k a' by default)
+- [Hotfix] Switched color config to use builtin 'colors' contributes property
+
 ## 0.2.0 - Aug 19, 2018
 - [Feature] Configuration for enabling / disabling annotations: jsannotations.enabled
 - [Feature] Configuration for changing color of annotations on light and dark themes: jsannotations.colors
--- src/annotationProvider.ts
+++ src/annotationProvider.ts
@@ -1,26 +1,15 @@
-import { Range, DecorationInstanceRenderOptions, DecorationOptions, workspace } from "vscode";
+import { Range, DecorationInstanceRenderOptions, DecorationOptions, workspace, ThemeColor } from "vscode";
 
 export class Annotations {
   static paramAnnotation(message: string, range: Range): DecorationOptions {
-    let lightColor = workspace.getConfiguration('jsannotations.colors').get('lightColor');
-    let darkColor = workspace.getConfiguration('jsannotations.colors').get('darkColor');
 
     return {
       range,
       renderOptions: {
-        light: {
-          before: {
-            color: lightColor
-          }
-        },
-        dark: {
-          before: {
-            color: darkColor
-          }
-        },
         before: {
           contentText: message,
-          fontStyle: "italic"
+          fontStyle: "italic",
+          color: new ThemeColor('jsannotations.annotationForeground')
         }
       } as DecorationInstanceRenderOptions
     } as DecorationOptions;
