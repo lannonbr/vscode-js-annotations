9f56496958c9e3b157e4e6e8a946cffdec057b70
Benjamin Lannon <benjamin@lannonbr.com>
2018-11-03 22:58:44 -0400

    Added script to run just unit tests

--- src/test/unit/paramExtractor.test.ts
+++ src/test/unit/paramExtractor.test.ts
@@ -2,7 +2,7 @@ import * as assert from "assert";
 import { splitToParamList } from "../../paramExtractor";
 
 suite("splitToParamList tests", () => {
-  test("should properly split simple parameter def with generic type", () => {
+  test("should properly split simple parameter def with generic type @unit", () => {
     const paramDef = "op1: Opt<T, A>";
     const expectedList = ["op1: Opt<T, A>"];
     const paramList = splitToParamList(paramDef);
@@ -10,7 +10,7 @@ suite("splitToParamList tests", () => {
     assert.deepEqual(paramList, expectedList);
   });
 
-  test("should properly split parameter def with nested generic type", () => {
+  test("should properly split parameter def with nested generic type @unit", () => {
     const paramDef = "op1: Opt<T, Test<A, B, C>, D>";
     const expectedList = ["op1: Opt<T, Test<A, B, C>, D>"];
     const paramList = splitToParamList(paramDef);
@@ -18,7 +18,7 @@ suite("splitToParamList tests", () => {
     assert.deepEqual(paramList, expectedList);
   });
 
-  test("should work with parameter def with arrow functions", () => {
+  test("should work with parameter def with arrow functions @unit", () => {
     const paramDef = "fn: (num: number, str: string) => string, opts: any";
     const expectedList = ["fn: (num: number, str: string) => string", "opts: any"];
     const paramList = splitToParamList(paramDef);
@@ -26,7 +26,7 @@ suite("splitToParamList tests", () => {
     assert.deepEqual(paramList, expectedList);
   });
 
-  test("should work with parameter def with arrow functions and generic return type", () => {
+  test("should work with parameter def with arrow functions and generic return type @unit", () => {
     const paramDef = "fn: (num: number, str: string) => Opt<A, B>, opts: any";
     const expectedList = ["fn: (num: number, str: string) => Opt<A, B>", "opts: any"];
     const paramList = splitToParamList(paramDef);
@@ -34,7 +34,7 @@ suite("splitToParamList tests", () => {
     assert.deepEqual(paramList, expectedList);
   });
 
-  test("should work with complex parameter def with arrow functions and generic return type", () => {
+  test("should work with complex parameter def with arrow functions and generic return type @unit", () => {
     const paramDef = "fn: (num: number, strOpt: Opt<A, B, C>) => Opt<A, B>, opts: any";
     const expectedList = ["fn: (num: number, strOpt: Opt<A, B, C>) => Opt<A, B>", "opts: any"];
     const paramList = splitToParamList(paramDef);
@@ -42,7 +42,7 @@ suite("splitToParamList tests", () => {
     assert.deepEqual(paramList, expectedList);
   });
 
-  test("should be fine with primitive types", () => {
+  test("should be fine with primitive types @unit", () => {
     const paramDef = "str: string, opts: any";
     const expectedList = ["str: string", "opts: any"];
     const paramList = splitToParamList(paramDef);
@@ -50,7 +50,7 @@ suite("splitToParamList tests", () => {
     assert.deepEqual(paramList, expectedList);
   });
 
-  test("should be fine with rest params", () => {
+  test("should be fine with rest params @unit", () => {
     const paramDef = "cats: number, ...rest: string[]";
     const expectedList = ["cats: number", "...rest: string[]"];
     const paramList = splitToParamList(paramDef);
